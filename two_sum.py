50b40eb36e47bf909267b36e0e47ccd90ad12485
Tobias <tobiasmailphone@gmail.com>
2024-05-04 23:55:22 +0200

    two sum solution

---
+++ two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_set = set()
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_set:
+                return [nums.index(complement), i]
+            num_set.add(num)
